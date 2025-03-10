d97449c8ed791f62c500790149d2e9d14da6781b
YogiboKorea <yogico.design@gmail.com>
2025-03-10 11:05:55 +0900

    dsfs

--- server.js
+++ server.js
@@ -202,14 +202,14 @@ async function getTop10ProductsByAddCart(providedDates) {
           product_name: finalName,
           displayText: `
             <div class="product-ranking" style="display: flex; align-items: center; gap: 10px; padding: 5px; border: 1px solid #ddd; border-radius: 5px;">
-              <div class="rank" style="font-weight: bold; color:rgb(105, 3, 8);">${index + 1}:</div>
+              <div class="rank" style="font-weight: bold; color:#e52e0a">${index + 1}</div>
               <div class="image" style="flex-shrink: 0;">
                 <img src="${listImage}" style="max-width:60px; width:60px; height:auto; border-radius: 5px;"/>
               </div>
               <div class="details" style="display: flex; flex-direction: column;">
                 <div class="product-name" style="font-weight:600; color: #333;">${finalName}</div>
                 <div class="product-count" style="font-size: 11px; color: #555;">
-                  총 <strong>${product.add_cart_count || 0}</strong> 개 상품이 장바구니에 담겨 있습니다.
+                  총 <strong>${product.add_cart_count || 0}</strong> 개 상품이 담겨 있습니다.
                 </div>
               </div>
             </div>
