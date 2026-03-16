6c5e159a02b179341d8c61429f0aabfcf78da0a0
sijoow <leshwann@gmail.com>
2026-03-16 10:35:34 +0900

    24

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ let db;
 const app = express();
 app.use(cors());
 app.use(compression());
+app.use(express.json({ limit: '50mb' }));                          
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
